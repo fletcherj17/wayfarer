8354f7a9e219585ea54e4120cdf188948362323e
Deval Shah <HirDev@devals-mbp.myfiosgateway.com>
2020-06-19 15:30:03 -0400

    Login and signup model are showing up on seperate modal now

--- main_app/static/jquery/script.js
+++ main_app/static/jquery/script.js
@@ -1,3 +1,6 @@
 $('#exampleModal').on('shown.bs.modal', function () {
     $('#myInput').trigger('focus')
   })
+  $('#example1Modal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+  })
