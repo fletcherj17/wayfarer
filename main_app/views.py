6c8c5efe652065af4b9c1cf67bfd9e683f449b55
chutto <christianhutto@gmail.com>
2020-06-22 15:37:25 -0500

    delete modal added

--- main_app/views.py
+++ main_app/views.py
@@ -34,7 +34,7 @@ def user_login(request):
     user = authenticate(request, username=username, password=password)
     if user is not None:
         login(request, user)
-        return redirect(f'/cities/8/')
+        return redirect('profile')
     else:
         return HttpResponse("Error")
 
